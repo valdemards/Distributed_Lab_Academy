d4b2998fdb8d944e6b00d4b479f5fe97c4d0c0c4
valdemards <62068685+valdemards@users.noreply.github.com>
2023-02-10 18:56:41 +0400

    stage_2

--- Baby_Blockchain/Stage_2/KeyPair.py
+++ Baby_Blockchain/Stage_2/KeyPair.py
@@ -1,26 +1,27 @@
 import rsa
 
-class KeyPair():
+
+class KeyPair:
 
     def __init__(self):
-        self.PublicKey = rsa.PublicKey
-        self.__PrivateKey = rsa.PrivateKey
+        self.public_key = rsa.PublicKey
+        self.__private_key = rsa.PrivateKey
 
-    def genKeyPair(self):
+    def gen_key_pair(self):
         key_pair = rsa.newkeys(16)
-        self.PublicKey = key_pair[0]
-        self.__PrivateKey = key_pair[1]
+        self.public_key = key_pair[0]
+        self.__private_key = key_pair[1]
         return (self)
 
-    def printKeyPair(self):
-        print(self.PublicKey.save_pkcs1(("PEM")).decode())
-        print(self.__PrivateKey.save_pkcs1(("PEM")).decode())
+    def print_key_pair(self):
+        print(self.public_key.save_pkcs1(("PEM")).decode())
+        print(self.__private_key.save_pkcs1(("PEM")).decode())
 
-    def toString(self):
-        return f"""{self.__PrivateKey.save_pkcs1(("PEM")).decode()}
-{self.PublicKey.save_pkcs1(("PEM")).decode()}"""
+    def to_string(self):
+        return f"""{self.__private_key.save_pkcs1(("PEM")).decode()}
+{self.public_key.save_pkcs1(("PEM")).decode()}"""
 
 
-new_keypair = KeyPair().genKeyPair()
-new_keypair.printKeyPair()
-print(new_keypair.toString())
+new_keypair = KeyPair().gen_key_pair()
+new_keypair.print_key_pair()
+print(new_keypair.to_string())
--- Baby_Blockchain/Stage_2/Signature.py
+++ Baby_Blockchain/Stage_2/Signature.py
@@ -1,19 +1,25 @@
 import rsa
 
-class Signature():
 
-    def signData(private_key, data):
+class Signature:
+
+    def sign_data(private_key, data):
         signature = rsa.sign(data.encode(), private_key, "SHA-256")
         return (signature)
 
-    def verifySignature(signature, data, public_key):
-        result = rsa.verify(data.encode(), signature, public_key)
-        return(result)
+    def verify_signature(signature, data, public_key):
+        try:
+            if isinstance(rsa.verify(data.encode(), signature, public_key), str):
+                return True
+            else:
+                return False
+        except:
+            return False
 
-    def toString(signature):
+    def to_string(signature):
         return f"""{signature}"""
 
-    def printSignature(signature):
+    def print_signature(signature):
         print(signature)
 
 
@@ -21,8 +27,14 @@ key_pair = rsa.newkeys(512)
 public_key = key_pair[0]
 private_key = key_pair[1]
 data = 'Hello cryptography!'
-signature = Signature.signData(private_key, data)
-print(Signature.verifySignature(signature, data, public_key))
-Signature.printSignature(signature)
-print(Signature.toString(signature))
+signature = Signature.sign_data(private_key, data)
 
+print('Testing positive case of verifying signature')
+print(Signature.verify_signature(signature, data, public_key))
+
+Signature.print_signature(signature)
+print(Signature.to_string(signature))
+
+print('Testing negative case of verifying signature after changing message')
+data = 'Good bye cryptography!!'
+print(Signature.verify_signature(signature, data, public_key))
