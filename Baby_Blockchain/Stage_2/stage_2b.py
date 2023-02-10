25d0a629362cfddcb03a932be5feb8098cbb8c7a
valdemards <62068685+valdemards@users.noreply.github.com>
2023-02-10 12:39:39 +0400

    KeyPair class

---
+++ Baby_Blockchain/Stage_2/stage_2b.py
@@ -0,0 +1,20 @@
+import rsa
+
+class KeyPair():
+
+    def __init__(self):
+        self.PublicKey = rsa.PublicKey
+        self.__PrivateKey = rsa.PrivateKey
+
+    def genKeyPair(self):
+        key_pair = rsa.newkeys(16)
+        self.PublicKey = key_pair[0]
+        self.__PrivateKey = key_pair[1]
+        return (self)
+
+    def printKeyPair(self):
+        print(self.PublicKey.save_pkcs1(("PEM")).decode())
+        print(self.__PrivateKey.save_pkcs1(("PEM")).decode())
+
+new_keypair = KeyPair().genKeyPair()
+new_keypair.printKeyPair()
