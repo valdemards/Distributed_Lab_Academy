965fa21880fa4fcc507927463938b11b2eb3aa69
valdemards <62068685+valdemards@users.noreply.github.com>
2023-02-17 11:02:45 +0400

    Added saving private and public keys to pem files

--- Baby_Blockchain/Stage_2/KeyPair.py
+++ Baby_Blockchain/Stage_2/KeyPair.py
@@ -11,15 +11,19 @@ class KeyPair:
         key_pair = rsa.newkeys(2048)
         self.public_key = key_pair[0]
         self.__private_key = key_pair[1]
-        return (self)
+        with open("private_key.pem", "wb") as f:
+            f.write(self.__private_key.save_pkcs1("PEM"))
+        with open("public_key.pem", "wb") as f:
+            f.write(self.public_key.save_pkcs1("PEM"))
+        return self
 
     def print_key_pair(self):
-        print(self.public_key.save_pkcs1(("PEM")).decode())
-        print(self.__private_key.save_pkcs1(("PEM")).decode())
+        print(self.public_key.save_pkcs1("PEM").decode())
+        print(self.__private_key.save_pkcs1("PEM").decode())
 
     def to_string(self):
-        return f"""{self.__private_key.save_pkcs1(("PEM")).decode()}
-{self.public_key.save_pkcs1(("PEM")).decode()}"""
+        return f"""{self.__private_key.save_pkcs1("PEM").decode()}
+{self.public_key.save_pkcs1("PEM").decode()}"""
 
 
 # new_keypair = KeyPair().gen_key_pair()
