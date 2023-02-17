d7571daefd13f98d5949efbcf4e81a07f7d29728
valdemards <62068685+valdemards@users.noreply.github.com>
2023-02-17 11:26:05 +0400

    Account class updated (only 1 key_pair per account allowed)

--- Baby_Blockchain/Stage_4/Operation.py
+++ Baby_Blockchain/Stage_4/Operation.py
@@ -1,5 +1,4 @@
 import rsa
-from Stage_2 import KeyPair
 from Stage_2 import Signature
 from Stage_3 import Account
 
@@ -21,13 +20,16 @@ class Operation:
 
     def verify_operation(self, signature, amount):
         try:
-            if self.receiver.get_balance() >= amount:
-                Signature.Signature().verify_signature(signature, self.receiver.public_key)
-            return True
+            if self.sender.get_balance() >= amount:
+                if Signature.Signature.verify_signature(signature, str(amount), self.sender.public_key) is True:
+                    return True
+                else:
+                    return False
+            else:
+                return False
         except Exception as e:
             print(e)
             return False
-
 
     def to_string(self):
         operation_string = 'sender = ' + str(self.sender.to_string()) + ' receiver = ' + str(self.receiver.to_string()) + ' amount = ' + str(self.amount) + ' signature = ' + str(self.signature)
@@ -40,28 +42,31 @@ class Operation:
         print('Signature ', self.signature)
 
 
-sender_key_pair = rsa.newkeys(512)
-sender_public_key = sender_key_pair[0]
-sender_private_key = sender_key_pair[1]
-
-receiver_key_pair = rsa.newkeys(512)
-receiver_public_key = receiver_key_pair[0]
-receiver_private_key = receiver_key_pair[1]
-
-
-# Creating new accounts:
-sender_account = Account.Account().create_account(sender_public_key)
-sender_account.update_balance(1)
-receiver_account = Account.Account().create_account(receiver_public_key)
-receiver_account.update_balance(0)
-
-amount = 1
-# Creating signature:
-sender_signature = Signature.Signature.sign_data(sender_private_key, str(amount))
-
-# Tests:
-
-operation_1 = Operation().create_operation(sender_account, receiver_account, amount, sender_signature)
-print(operation_1.to_string())
+# # Creating key pairs for sender and receiver
+# sender_key_pair = rsa.newkeys(2048)
+# sender_public_key = sender_key_pair[0]
+# sender_private_key = sender_key_pair[1]
+#
+# receiver_key_pair = rsa.newkeys(2048)
+# receiver_public_key = receiver_key_pair[0]
+# receiver_private_key = receiver_key_pair[1]
+#
+# # Creating new accounts:
+# sender_account = Account.Account().create_account(sender_public_key)
+# sender_account.update_balance(1)
+# receiver_account = Account.Account().create_account(receiver_public_key)
+# receiver_account.update_balance(0)
+#
+# # Creating signature:
+# amount = 1
+# sender_signature = Signature.Signature.sign_data(sender_private_key, str(amount))
+#
+# # Tests:
+# operation_1 = Operation().create_operation(sender_account, receiver_account, amount, sender_signature)
+# print(operation_1.to_string())
 # operation_1.print()
-print(operation_1.verify_operation(sender_signature, amount))
+# # # Positive scenario
+# print(operation_1.verify_operation(sender_signature, amount))
+# # Negative scenario
+# amount = 2
+# print(operation_1.verify_operation(sender_signature, amount))
